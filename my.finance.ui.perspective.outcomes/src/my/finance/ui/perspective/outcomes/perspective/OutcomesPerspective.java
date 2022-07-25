473a0f8563491187e7565034148660633d9ba32b
Elena Larina <elena.larina.1@gmail.com>
2022-07-25 09:55:00 +0400

    Remove bin folders

---
+++ my.finance.ui.perspective.outcomes/src/my/finance/ui/perspective/outcomes/perspective/OutcomesPerspective.java
@@ -0,0 +1,13 @@
+package my.finance.ui.perspective.outcomes.perspective;
+import org.eclipse.ui.IPageLayout;
+import org.eclipse.ui.IPerspectiveFactory;
+
+public class OutcomesPerspective implements IPerspectiveFactory {
+
+	@Override
+	public void createInitialLayout(IPageLayout layout) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
