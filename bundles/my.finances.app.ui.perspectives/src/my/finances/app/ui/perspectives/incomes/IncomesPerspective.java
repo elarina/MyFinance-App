069c55b39febd7a70781051a157c5bec6fd32932
elarina <elena.larina.1@gmail.com>
2022-07-29 00:28:47 +0400

    Configure views

--- bundles/my.finances.app.ui.perspectives/src/my/finances/app/ui/perspectives/incomes/IncomesPerspective.java
+++ bundles/my.finances.app.ui.perspectives/src/my/finances/app/ui/perspectives/incomes/IncomesPerspective.java
@@ -3,13 +3,16 @@ package my.finances.app.ui.perspectives.incomes;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import my.finances.app.ui.views.views.IncomesView;
+
 public class IncomesPerspective implements IPerspectiveFactory {
 
 	public static final String ID = "my.finances.app.incomes_perspective";
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
 	}
 
 }
--- bundles/my.finances.app.ui.perspectives/src/my/finances/app/ui/perspectives/templates/TepmlatesPerspective.java
+++ bundles/my.finances.app.ui.perspectives/src/my/finances/app/ui/perspectives/templates/TepmlatesPerspective.java
@@ -3,12 +3,16 @@ package my.finances.app.ui.perspectives.templates;
 import org.eclipse.ui.IPageLayout;
 import org.eclipse.ui.IPerspectiveFactory;
 
+import my.finances.app.ui.views.views.IncomesView;
+import my.finances.app.ui.views.views.OutcomesView;
+import my.finances.app.ui.views.views.PeriodsView;
+
 public class TepmlatesPerspective implements IPerspectiveFactory {
 
 	@Override
 	public void createInitialLayout(IPageLayout layout) {
-		// TODO Auto-generated method stub
-
+		layout.setEditorAreaVisible(false);
+		layout.setFixed(true);
 	}
 
 }
